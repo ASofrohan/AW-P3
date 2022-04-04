48ed8ddea561d9cfbf1ec55abdb4e34b76f5c2d5
fernas08 <fernas08@ucm.es>
2022-04-04 17:48:20 +0200

    pizzas

---
+++ includes/src/FormularioPersonalizada.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__.'/Form.php';
+//require_once __DIR__.'/Aplicacion.php';
+require_once __DIR__.'/Pizzas.php';
+
+class FormularioPesonalizada extends Form
+{
+    public function __construct() {
+        parent::__construct('formPersonalizada');
+    }
+    
+    public function formulario(){
+        $ingredientes = Pizzas::getIngredientes();
+        $masas = Pizzas::getMasas();
+        $tamnios = Pizzas::getTamanios();
+
+        echo '<p>INGREDIENTES: </p>';
+        echo '<form action="#" method="post" id=ingredientes_cb>';
+        while($row = $ingredientes->fetch_assoc()) {
+            echo '<input type="checkbox" name="ingredientes[]" value="' . $row['Precio'] . '" id="' . $row['ID_Ingrediente'] . '" onClick=recalcularPrecio(this) /> ' . $row['Nombre'] . '</br>';
+        }
+        echo '</form>';
+    }
+}
+?>
---
+++ includes/src/Pizzas.php
@@ -0,0 +1,46 @@
+<?php
+require_once __DIR__.'/Aplicacion.php';
+
+class Pizzas
+{
+    private $id_pizza;
+    private $precio;
+    private $personalizada;
+    private $nombre;
+
+    private function __construct($id_pizza, $precio, $personalizada, $nombre)
+    {
+        $this->$id_pizza = $id_pizza;
+        $this->$precio = $precio;
+        $this->$personalizada = $personalizada;
+        $this->$nombre = $nombre;
+    }
+
+    public static function getIngredientes(){
+        $app = Aplicacion::getInstancia();
+        $conn = $app->conexionBd();
+        $query = "SELECT * FROM ingredientes";
+        $resultado=$conn->query($query);
+
+        return $resultado;
+    }
+
+    public static function getMasas(){
+        $app = Aplicacion::getInstancia();
+        $conn = $app->conexionBd();
+        $query = "SELECT * FROM masas";
+        $resultado=$conn->query($query);
+
+        return $resultado;
+    }
+
+    public static function getTamanio(){
+        $app = Aplicacion::getInstancia();
+        $conn = $app->conexionBd();
+        $query = "SELECT * FROM tamaños";
+        $resultado=$conn->query($query);
+
+        return $resultado;
+    }
+}
+
