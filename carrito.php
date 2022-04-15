b9f62df377159eb34f6fd95e67beb073f2d623f1
MarcosPadillaAl <83251213+MarcosPadillaAl@users.noreply.github.com>
2022-04-15 23:50:12 +0200

    carrito casi definitivo

--- carrito.php
+++ carrito.php
@@ -6,16 +6,17 @@ require_once __DIR__.'/includes/src/Carrito.php';
 require_once __DIR__.'/includes/src/Aplicacion.php';
 
 $carrito= new Carrito();
-
 $nombre=$carrito->inicio();
+$string=mostrar($nombre,$carrito);
+
 $tituloPagina = 'Carrito';
-?>
+$contenidoPrincipal=<<<EOF
+	$string
+EOF;
+include __DIR__.'/includes/vistas/plantillas/plantilla.php';
 
-<link rel="stylesheet" type="text/css" href="css/estilo.css" />
-
-<div id="carrito">
-
-		<?php
+function mostrar($nombre,$carrito){
+	$carritoToString="";
 	if($nombre!=null){
 		$arrayPyB= $carrito->consultaPedidosBebPiz();
 		$precio=$carrito->consultaPrecio();
@@ -23,59 +24,61 @@ $tituloPagina = 'Carrito';
 		$arrayPP=$carrito->consultaPersonalizada();
 		$descuento=$carrito->consultaDescuento();
 		$oferta=null;
-		////
+		
+		
 		if($arrayPyB!=null){
 			$arrlength = count($arrayPyB);
-			echo'<a>Pedidos por pagar por = '.$nombre.' </a>';
-			echo'<br></br>';
+			$carritoToString=$carritoToString.'Pedidos por pagar por = '.$nombre.' </a>'.'<br></br>';
 			for($x = 0; $x < $arrlength-1; $x++) {
-				echo $arrayPyB[$x].'  ';
-				echo "<br>";
+				$carritoToString=$carritoToString. $arrayPyB[$x].'  ';
+				if($x%2==1)
+				$carritoToString=$carritoToString.'<br>';
 			}
 		
 			$oferta=$arrayPyB[$arrlength-1];
 		}
-		echo'<br></br>';
+		$carritoToString=$carritoToString.'<br></br>';
 		if($arrayPP!=null){
 			$arrlength = count($arrayPP);
 			if($arrlength==0){
-				echo 'No hay pizzas personalizadas';
+				$carritoToString=$carritoToString.'No hay pizzas personalizadas';
 				$sumTot=$precio;
 			}
 			else{
-				echo 'Pizzas personalizadas <br>';
+				$carritoToString=$carritoToString.'Pizzas personalizadas <br>';
 				for($x = 0; $x < $arrlength; $x++) {
-					echo $arrayPP[$x].'  ';
-					echo "<br>";
+					$carritoToString=$carritoToString.$arrayPP[$x].'  ';
+					if($x%2==1)
+					$carritoToString=$carritoToString.'<br>';
 				}
-				echo "<br>";
+				$carritoToString=$carritoToString."<br>";
 				$sumTot=$precio+$precioP;
 			}
 		}else{
 			$sumTot=$precio;
-			echo 'No hay pizzas personalizadas'.'<br>';
+			$carritoToString=$carritoToString.'No hay pizzas personalizadas'.'<br>';
 		}
 		if($oferta!=null){
 			if($oferta==4){
-				echo'Precio Total a pagar '.$sumTot.' €';
-				echo'<br></br>';
-				echo'<a>Tienes ofertas ¡Usalas!</a>';
-				echo'<a href=" oferta.php">Canjear  </a><br>';			
+				$carritoToString=$carritoToString.'Precio Total a pagar '.$sumTot.' €';
+				$carritoToString=$carritoToString.'<br></br>';
+				$carritoToString=$carritoToString.'<a>Tienes ofertas ¡Usalas!</a>';
+				$carritoToString=$carritoToString.'<a href=" oferta.php">Canjear  </a><br>';			
 			}else{
 				if($descuento!=0){
-					echo '<strike>Precio Total a pagar '.$sumTot.'€</strike>';
-					echo'<br>';
-					echo'Precio nuevo a pagar '.$sumTot-$descuento.'€';
+					$carritoToString=$carritoToString. '<strike>Precio Total a pagar '.$sumTot.'€</strike>';
+					$carritoToString=$carritoToString.'<br>';
+					$carritoToString=$carritoToString.'Precio nuevo a pagar '.$sumTot-$descuento.'€';
 				}
 			}
-			echo'<br>';
-				echo'<a>¿Quieres editar el pedido?</a><a href=procesarEdit.php><button>EDITAR</button></a><br>';
-				echo'<a href=procesarCompra.php><button>Comprar</button></a>';
+			$carritoToString=$carritoToString.'<br>';
+			$carritoToString=$carritoToString.'<a>¿Quieres editar el pedido?</a><a href=procesarEdit.php><button>EDITAR</button></a><br>';
+			$carritoToString=$carritoToString.'<a href=procesarCompra.php><button>Comprar</button></a>';
 		}
 		
 	}
 	else{
-		echo'No estas registrado';
+		$carritoToString=$carritoToString.'No estas registrado';
 	}
-		?>
-</div>
+	return $carritoToString;
+}	
