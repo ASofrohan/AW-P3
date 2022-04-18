1d7cd24e53cb648244d647803c84cd3903eaef10
mmiralle17 <mmiralle@ucm.es>
2022-04-18 10:30:30 +0000

    Update Bebidas.php

--- includes/src/Bebidas.php
+++ includes/src/Bebidas.php
@@ -14,6 +14,52 @@ class Bebidas
         $this->precio = $precio;
     }
 
+    public static function getBebidas(){
+        $app = Aplicacion::getInstancia();
+        $conn = $app->conexionBd();
+        $query = "SELECT * FROM bebidas";
+        $resultado=$conn->query($query);
+
+        return $resultado;
+    }
+
+    public static function muestraPizzas(){
+        $bebidas = self::getBebidas();
+        $arrayBebidas = array();
+
+        if(mysqli_num_rows($bebidas)>0){
+            $i=0;
+            while($row = mysqli_fetch_assoc($bebidas)) {
+                
+                $id_bebida=$row['ID_Bebida'];
+                $precio=$row['Precio'];
+                $nombre=$row['Nombre'];
+                $image=$row['Image'];
+                $b = new Bebidas($id_bebida, $precio, $nombre, $image);
+                $arrayBebidas[$i] = $b;
+                $i += 1;
+            }
+        }
+        return $arrayBebidas;
+    }
+
+    public function get_id()
+    {
+        return $this->id_bebida;
+    }
+    public function get_precio()
+    {
+        return $this->precio;
+    }
+    public function get_nombre()
+    {
+        return $this->nombre;
+    }
+    public function get_image()
+    {
+        return $this->image;
+    }
+
 }
 
 
