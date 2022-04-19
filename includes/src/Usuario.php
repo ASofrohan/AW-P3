6f45c4b89ab119b5d87df3470135a3e0b779019c
helio-O <hotero@ucm.es>
2022-04-19 18:15:47 +0200

    Update Usuario.php

--- includes/src/Usuario.php
+++ includes/src/Usuario.php
@@ -52,14 +52,14 @@ class Usuario
     {
         $app = Aplicacion::getInstancia();
         $conn = $app->conexionBd();
-        $query = sprintf("SELECT * FROM Usuarios U WHERE U.Correo = '%s'", $conn->real_escape_string($correo));
+        $query = sprintf("SELECT * FROM usuarios U WHERE U.Correo = '%s'", $conn->real_escape_string($correo));
         $rs = $conn->query($query);
         $result = false;
         if ($rs) {
             if ( $rs->num_rows == 1) {
                 $fila = $rs->fetch_assoc();
                 $dom=$fila['Domicilio'];
-                $query = sprintf("SELECT * FROM Domicilios WHERE ID_Domicilio = '$dom'");
+                $query = sprintf("SELECT * FROM domicilios WHERE ID_Domicilio = '$dom'");
              $resultado=$conn->query($query);
              $row = $resultado->fetch_assoc();
                 $user = new Usuario($fila['Correo'], $fila['Nombre'], $fila['Apellidos'], $fila['Contraseña'], $row['Calle'],$row['Ciudad'],$row['Piso'],$row['CodigoPostal']);
@@ -111,7 +111,7 @@ class Usuario
              $resultado=$conn->query($query);
              $row = $resultado->fetch_assoc();
              $id = $row["ID_Domicilio"];
-             $query = sprintf("INSERT INTO Usuarios (Correo, Nombre, Apellidos,Contraseña, Admin, Domicilio) VALUES ('%s','%s','%s','%s','0','$id')"
+             $query = sprintf("INSERT INTO usuarios (Correo, Nombre, Apellidos,Contraseña, Admin, Domicilio) VALUES ('%s','%s','%s','%s','0','$id')"
              , $conn->real_escape_string($usuario->correo)
              , $conn->real_escape_string($usuario->nombre)
              , $conn->real_escape_string($usuario->apellidos)
