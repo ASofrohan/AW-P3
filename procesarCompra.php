28402f9158479bc0a957ea66c552e4a6e5051eea
MarcosPadillaAl <83251213+MarcosPadillaAl@users.noreply.github.com>
2022-04-19 12:55:57 +0200

    Update procesarCompra.php

--- procesarCompra.php
+++ procesarCompra.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__.'/includes/config.php';
-        $app = Aplicacion::getInstancia();
+$app = Aplicacion::getInstancia();
         $db = $app->conexionBd();
         
         $co=$_SESSION['correo'];
 
         $query="UPDATE pedidos
         SET Estado=0
-        WHERE Usuario ='$co' AND ID_Pedido=1";//cambiar id pedido
+        WHERE Usuario ='$co' AND Estado=1";//cambiar id pedido
         $resultado=$db->query($query);
        
  ?>
