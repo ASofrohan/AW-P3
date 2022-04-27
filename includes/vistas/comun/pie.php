55be2c2d6718cdd359a58d298bc4732306db0932
fernas08 <fernas08@ucm.es>
2022-04-27 23:21:39 +0200

    footer

--- includes/vistas/comun/pie.php
+++ includes/vistas/comun/pie.php
@@ -1,3 +1,34 @@
-<footer>
-		Pie de página
-	</footer>
+<footer class="text-center footer-style">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 footer-col">
+                <h3>Dirección</h3>
+                <p>
+                    UCM - Facultad de Informática <br/>
+                    Calle del Prof. José García Santesmases, 9, 28040 Madrid
+                </p>
+            </div>
+            <div class="col-md-4 footer-col">
+                <h3>Redes</h3>
+                <ul class="list-inline">
+					<div class="ops-v">
+						<img src="images/redes/twitter.png" alt="twitter"  WIDTH=25 HEIGHT=25> Twitter
+					</div>
+					<div class="ops-v">
+						<img src="images/redes/facebook.png" alt="twitter"  WIDTH=25 HEIGHT=25> Facebook
+					</div>
+					<div class="ops-v">
+						<img src="images/redes/instagram.jpg" alt="twitter"  WIDTH=25 HEIGHT=25> Instagram
+					</div>
+					<div class="ops-v">
+						<img src="images/redes/youtube.jpg" alt="twitter"  WIDTH=25 HEIGHT=25> YouTube
+					</div>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-col">
+                <h3>Sobre Nosotros</h3>
+                <p>Somos un grupo de alumnos que hemos desarollado esta pagina web en la asignatura de Aplicaciones Web</p>
+            </div>
+        </div>
+    </div>
+</footer>
