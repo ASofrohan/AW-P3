abd9fe5e13f0d6d96c081b5fec1aa217b5136612
fernas08 <fernas08@ucm.es>
2022-04-28 18:04:24 +0200

    Menu

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -3,19 +3,30 @@
 		<h1>PizzaGuay</h1>
 		<?php
 		if (!isset($_SESSION["esAdmin"])) {
-			
-			echo '<a href="index.php">INICIO</a>
-			<a href="pizzas.php">PIZZAS</a>                    
-			<a href="ofertas.php">OFERTAS</a>
-			<a href="bebidas.php">BEBIDAS</a>
-			<a href="foro.php">RESEÑAS</a>';
+			echo'
+			<input type="checkbox" class="checkbox" id="menu-toogle"/>
+			<label for="menu-toogle" class="menu-toogle"></label>
+			<nav class="nav">
+			  <a href="index.php" class="nav__item current">Inicio</a>
+			  <a href="pizzas.php" class="nav__item">Pizzas</a>
+			  <a href="ofertas.php" class="nav__item">Ofertas</a>
+			  <a href="bebidas.php" class="nav__item">Bebidas</a>
+			  <a href="foro.php" class="nav__item">Reseñas</a>
+			</nav>
+			';
 		} else{
-			echo '<a href="index.php">INICIO</a>
-			<a href="pizzas.php">PIZZAS</a>                    
-			<a href="ofertas.php">OFERTAS</a>
-			<a href="bebidas.php">BEBIDAS</a>
-			<a href="foro.php">RESEÑAS</a>
-			<a href="administrar.php">ADMINISTRAR</a>';
+			echo'
+			<input type="checkbox" class="checkbox" id="menu-toogle"/>
+			<label for="menu-toogle" class="menu-toogle"></label>
+			<nav class="nav">
+			  <a href="index.php" class="nav__item current">Inicio</a>
+			  <a href="pizzas.php" class="nav__item">Pizzas</a>
+			  <a href="ofertas.php" class="nav__item">Ofertas</a>
+			  <a href="bebidas.php" class="nav__item">Bebidas</a>
+			  <a href="foro.php" class="nav__item">Reseñas</a>
+			  <a href="administrar.php">Administar</a>
+			</nav>
+			';
 		}
 		?>
 		<?php
