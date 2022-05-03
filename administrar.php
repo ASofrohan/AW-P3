c914da0872513d41bf5949895d264edcc16d6286
MarcosPadillaAl <83251213+MarcosPadillaAl@users.noreply.github.com>
2022-05-03 12:57:53 +0200

    .

--- administrar.php
+++ administrar.php
@@ -3,15 +3,16 @@
 require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Administracion';
-
+$string="";
 $contenidoPrincipal =
-<<<EOF
-	Hola administrador!!.<Br>
-    A continuacion te mostramos lo que puedes hacer:<Br>
-    <Br>
-    (Poned descripcion de lo que puede hacer el admin en cada funcionalidad que un usuario normal no pueda)<Br>
-    -Editar o borrar cualquier mensaje en el foro a parte de lo que podria hacer un usuario normal.<Br>
-    -Borrar ,añadir pedidos y modificar el estado del pedido.
-EOF;
+
+$string=$string.'Hola administrador!!'.'<Br>';
+$string=$string. 'A continuacion te mostramos lo que puedes hacer:'.'<Br>';
+$string=$string. '<Br>';
+$string=$string. '(Poned descripcion de lo que puede hacer el admin en cada funcionalidad que un usuario normal no pueda)'.'<Br>';
+$string=$string.  '-Editar o borrar cualquier mensaje en el foro a parte de lo que podria hacer un usuario normal.'.'<Br>';
+$string=$string.  '-Borrar ,añadir pedidos y modificar el estado del pedido.';
+$string=$string.  mostrarGestion();
+
 
 include __DIR__.'/includes/vistas/plantillas/plantilla.php';
